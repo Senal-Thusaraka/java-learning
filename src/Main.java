d4bb9226a5f0e792e4b0a7866e56fba5e5e80442
Senal Thusaraka <senalthusaraka14@gmail.com>
2025-12-18 22:40:47 +0530

    Initial project setup

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String z ="Nico is the best";
         System.out.println(z);
+        System.out.println("No.. Nico is the Greatest"):
         //Dev by Nico_lk
     }
 }
